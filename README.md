6951cdf6648f01fe57ba5d1f054ebf464cad5957
Hadrien Reynaud <hjr119@ryuk.doc.ic.ac.uk>
2021-10-25 18:53:53 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Vision Transformer Segmentation Network
+
+This implementation of [`ViT`](https://arxiv.org/abs/2010.11929) in pytorch uses a super simple and straight-forward way of generating an output of the same size as the input by applying the inverse rearrange operation on all the predicted outputs. This enables convolution-free multi-class segmentation.
+
+Most of the code is taken from https://github.com/lucidrains/vit-pytorch/blob/main/vit_pytorch/vit.py
+
+<p align="center">
+<img src="images/ViTSeg.png" width=80%>
+</p>
+
+## Default Architecture Parameters:
+
+```
+model = ViTSeg( image_size=112, 
+                channels=1,
+                patch_size=7, 
+                num_classes=1, 
+                dim=768, 
+                depth=6, 
+                heads=12, 
+                mlp_dim=2048, 
+                learned_pos=False, 
+                use_token=False)
+```
+
+- ```image_size```: An integer or a tuple defining the size of the input image (some code rewrite would enable any image size to be passed)
+- ```channels```: An integer defining the umber of channels in the input image
+- ```patch_size```: An integer or a tuple defining the size of the patches
+- ```num_classes```: An integer representing the nuber of channels in the ouput 
+- ```dim```: An integer defining the size of the embedding dimension
+- ```depth```: An integer defining the number of transformer layers
+- ```heads```: An integer defining the number of heads in the transformer layers
+- ```mlp_dim```: An integer defining the size of the MLP in the transformer layers
+- ```learned_pos```: A boolean which, if true, switches from fixed positional encoding to learned positional encodings
+- ```use_token```: A boolean which, if true, add a CLS token in the input and output
+
+
+## Citation
+If you find this repository useful, please consider citing it:
+```
+@article{reynaud2021vitseg,
+  title={ViTSeg-https://github.com/HReynaud/ViTSeg}, 
+  url={https://github.com/HReynaud/ViTSeg},  
+  Author={Reynaud, Hadrien}, 
+  Year={2021"}
+}
+```
---
+++ ViT.py
@@ -0,0 +1,235 @@
+# This code was taken from https://github.com/lucidrains/vit-pytorch/blob/main/vit_pytorch/vit.py
+
+from matplotlib import use
+import torch
+from torch import nn
+
+import numpy as np
+
+from einops import rearrange, repeat
+from einops.layers.torch import Rearrange
+from torch.nn.modules.activation import Sigmoid
+
+from torchsummary import summary
+
+
+def pair(t):
+    return t if isinstance(t, tuple) else (t, t)
+
+
+def get_angles(pos, i, d_model):
+    angle_rates = 1 / np.power(10000, (2 * (i//2)) / np.float32(d_model))
+    return pos * angle_rates
+
+
+def positional_encoding(position, d_model):
+    angle_rads = get_angles(np.arange(position)[:, np.newaxis],
+                            np.arange(d_model)[np.newaxis, :],
+                            d_model)
+
+    # apply sin to even indices in the array; 2i
+    angle_rads[:, 0::2] = np.sin(angle_rads[:, 0::2])
+
+    # apply cos to odd indices in the array; 2i+1
+    angle_rads[:, 1::2] = np.cos(angle_rads[:, 1::2])
+
+    pos_encoding = angle_rads[np.newaxis, ...]
+
+    return torch.tensor(pos_encoding, dtype=torch.float32)
+
+
+class PreNorm(nn.Module):
+    def __init__(self, dim, fn):
+        super().__init__()
+        self.norm = nn.LayerNorm(dim)
+        self.fn = fn
+    def forward(self, x, **kwargs):
+        return self.fn(self.norm(x), **kwargs)
+
+
+class FeedForward(nn.Module):
+    def __init__(self, dim, hidden_dim, dropout = 0.):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(dim, hidden_dim),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(hidden_dim, dim),
+            nn.Dropout(dropout)
+        )
+    def forward(self, x):
+        return self.net(x)
+
+
+class Attention(nn.Module):
+    def __init__(self, dim, heads = 8, dim_head = 64, dropout = 0.):
+        super().__init__()
+        inner_dim = dim_head *  heads
+        project_out = not (heads == 1 and dim_head == dim)
+
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+
+        self.attend = nn.Softmax(dim = -1)
+        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias = False)
+
+        self.to_out = nn.Sequential(
+            nn.Linear(inner_dim, dim),
+            nn.Dropout(dropout)
+        ) if project_out else nn.Identity()
+
+    def forward(self, x):
+        qkv = self.to_qkv(x).chunk(3, dim = -1)
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = self.heads), qkv)
+
+        dots = torch.matmul(q, k.transpose(-1, -2)) * self.scale
+
+        attn = self.attend(dots)
+
+        out = torch.matmul(attn, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        return self.to_out(out)
+
+
+class Transformer(nn.Module):
+    def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout = 0.):
+        super().__init__()
+        self.layers = nn.ModuleList([])
+        for _ in range(depth):
+            self.layers.append(nn.ModuleList([
+                PreNorm(dim, Attention(dim, heads = heads, dim_head = dim_head, dropout = dropout)),
+                PreNorm(dim, FeedForward(dim, mlp_dim, dropout = dropout))
+            ]))
+    def forward(self, x):
+        for attn, ff in self.layers:
+            x = attn(x) + x
+            x = ff(x) + x
+        return x
+
+
+class ViT(nn.Module):
+    def __init__(self, *, image_size, patch_size, num_classes, dim, depth, heads, mlp_dim, pool = 'cls', channels = 3, dim_head = 64, dropout = 0., emb_dropout = 0.):
+        super().__init__()
+        image_height, image_width = pair(image_size)
+        patch_height, patch_width = pair(patch_size)
+
+        assert image_height % patch_height == 0 and image_width % patch_width == 0, 'Image dimensions must be divisible by the patch size.'
+
+        num_patches = (image_height // patch_height) * (image_width // patch_width)
+        patch_dim = channels * patch_height * patch_width
+        assert pool in {'cls', 'mean'}, 'pool type must be either cls (cls token) or mean (mean pooling)'
+
+        self.to_patch_embedding = nn.Sequential(
+            Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1 = patch_height, p2 = patch_width),
+            nn.Linear(patch_dim, dim),
+        )
+
+        self.pos_embedding = nn.Parameter(torch.randn(1, num_patches + 1, dim))
+        self.cls_token = nn.Parameter(torch.randn(1, 1, dim))
+        self.dropout = nn.Dropout(emb_dropout)
+
+        self.transformer = Transformer(dim, depth, heads, dim_head, mlp_dim, dropout)
+
+        self.pool = pool
+        self.to_latent = nn.Identity()
+
+        self.mlp_head = nn.Sequential(
+            nn.LayerNorm(dim),
+            nn.Linear(dim, num_classes),
+            nn.Sigmoid()
+        )
+
+    def forward(self, img):
+        x = self.to_patch_embedding(img)
+        b, n, _ = x.shape
+
+        cls_tokens = repeat(self.cls_token, '() n d -> b n d', b = b)
+        x = torch.cat((cls_tokens, x), dim=1)
+        x += self.pos_embedding[:, :(n + 1)]
+        x = self.dropout(x)
+
+        x = self.transformer(x)
+
+        x = x.mean(dim = 1) if self.pool == 'mean' else x[:, 0]
+
+        x = self.to_latent(x)
+        return self.mlp_head(x)
+
+
+class ViTSeg(nn.Module):
+    def __init__(self, *, image_size, patch_size, num_classes, dim, depth, heads, mlp_dim, 
+                        pool = 'cls', channels = 3, dim_head = 64, dropout = 0., emb_dropout = 0., 
+                        learned_pos=True, use_token=False):
+        super().__init__()
+        image_height, image_width = pair(image_size)
+        patch_height, patch_width = pair(patch_size)
+        self.use_token = use_token
+
+        assert image_height % patch_height == 0 and image_width % patch_width == 0, 'Image dimensions must be divisible by the patch size.'
+
+        num_patches = (image_height // patch_height) * (image_width // patch_width)
+        patch_dim = channels * patch_height * patch_width
+        assert pool in {'cls', 'mean'}, 'pool type must be either cls (cls token) or mean (mean pooling)'
+
+        self.to_patch_embedding = nn.Sequential(
+            Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1 = patch_height, p2 = patch_width),
+            nn.Linear(patch_dim, dim),
+        )
+
+        self.pos_embedding = nn.Parameter(torch.randn(1, num_patches + 1, dim)) if learned_pos else nn.Parameter(positional_encoding(2048, dim), requires_grad=False)
+        if self.use_token:
+            self.cls_token = nn.Parameter(torch.randn(1, 1, dim))
+        self.dropout = nn.Dropout(emb_dropout)
+
+        self.transformer = Transformer(dim, depth, heads, dim_head, mlp_dim, dropout)
+
+        self.pool = pool
+        self.to_latent = nn.Identity()
+
+        self.mlp_head = nn.Sequential(
+            nn.LayerNorm(dim),
+            nn.Linear(dim, num_classes),
+            nn.Sigmoid()
+        )
+
+        self.to_reconstructed = nn.Sequential(
+            nn.Linear(dim, patch_height * patch_width),
+            Rearrange('b (h w) (p1 p2 c) -> b c (h p1) (w p2)', h = (image_height // patch_height), w = (image_width // patch_width), p1 = patch_height, p2 = patch_width, c=1),
+        )
+
+        self.to_regression = nn.Sequential(
+            nn.Linear(dim, 1),
+        )
+
+    def forward(self, img):
+        x = self.to_patch_embedding(img)
+        b, n, _ = x.shape
+
+        if self.use_token:
+            cls_tokens = repeat(self.cls_token, '() n d -> b n d', b = b)
+            x = torch.cat((cls_tokens, x), dim=1)
+
+        x += self.pos_embedding[:, :n+self.use_token]
+        x = self.dropout(x)
+
+        x = self.transformer(x)
+
+        if self.use_token:
+            cls_tokens = x[:, 0, :]
+            x = x[:, 1:, :]
+            cls_tokens = self.to_regression(cls_tokens)
+
+        x = self.to_reconstructed(x)
+        
+        return {"out":x, "token":cls_tokens}
+
+
+if __name__ == "__main__":
+
+    model = ViTSeg(image_size=112, patch_size=7, num_classes=1, dim=768, depth=6, heads=12, mlp_dim=2048, channels=1,
+                    learned_pos=False, use_token=True)
+    
+    summary(model, (1, 112, 112), device='cpu')
+
+
+    
